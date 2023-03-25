c7f83adfe735802f4dd63b2e06c8fa15237d6ef3
Yevgeniy Wang <yevgeniy.wang@gmail.com>
2023-03-25 22:16:15 +0200

    first commit

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+/* Когда пользователь нажимает на кнопку, переключаться раскрывает содержимое */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
